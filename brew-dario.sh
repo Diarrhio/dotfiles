390ab9b1dc70df5ac470ded9131b3790476947a9
Dario Ahdoot <diarrhio@gmail.com>
2018-04-05 22:43:59 -0700

    Add my own brew.sh

---
+++ brew-dario.sh
@@ -0,0 +1,32 @@
+brew install python@2
+brew install pipenv
+brew install ruby
+brew install git
+brew install mercurial
+brew install mas
+brew install nativefier
+brew install coreutils
+brew install ccache
+brew install p7zip
+brew install pcre
+brew install tree
+
+brew cask install tortoisehg
+brew cask install sourcetree
+brew cask install diffmerge
+brew cask install cmake
+brew cask install macdown
+
+brew cask install parallels
+brew cask install dropbox
+brew cask install google-backup-and-sync
+brew cask install deluge
+brew cask install vlc
+
+brew cask install betterzip
+brew cask install provisionql
+brew cask install qlcolorcode
+brew cask install qlmarkdown
+brew cask install qlstephen
+brew cask install quicklook-json
+brew cask install quicklookapk
